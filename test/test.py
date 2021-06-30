b80c6344e6aa5a2441e5e4a298cbd96017ff12ad
ct <2695562503@qq.com>
2021-06-30 13:40:47 +0800

    update

--- test/test.py
+++ test/test.py
@@ -4,7 +4,8 @@ import yaml
 
 class TestDemo:
     def test_url(self):
+        base_url = yaml.load(open("data.yaml"))["pangu_dev"]
         data = json.dumps({"pageCount": 20, "pageNumber": 1, "pageIs": True})
         headers = {"Content-Type": "application/json;charset=UTF-8"}
-        r = requests.post("http://pangu-dev.healthdt.cn/ht-pangu-manager/web/project/page", data=data, headers=headers)
+        r = requests.post(base_url+"/ht-pangu-manager/web/project/page", data=data, headers=headers)
         assert r.json()["code"] == 2000
