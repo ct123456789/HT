528024b594c2c7d86b3cf8d0ba2b8d142b3c881f
ct <2695562503@qq.com>
2021-07-02 14:19:29 +0800

    update

--- test_case/test.py
+++ test_case/test.py
@@ -1,14 +1,14 @@
-import os
 
-import pytest
 import requests
 import json
 import yaml
 import allure
 
+
+@allure.feature('请求项目管理分页查询接口')
 class TestDemo:
 
-    @allure.feature('请求项目管理分页查询接口')
+    @allure.story('查询项目管理')
     def test_url(self):
         base_url = yaml.load(open("data.yaml"))["pangu_dev"]
         data = json.dumps({"pageCount": 20, "pageNumber": 1, "pageIs": True})
