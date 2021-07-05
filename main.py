1d88ccb7fcfa578035f91034995918326dd01eb1
ct <2695562503@qq.com>
2021-07-05 15:31:48 +0800

    update

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+import pytest
 
 
 def print_hi(name):
@@ -12,5 +14,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    pytest.main(["./"])
+    os.system(f"allure generate report/result -o report/allure_html --clean")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- test_case/test.py
+++ test_case/test.py
@@ -4,13 +4,15 @@ import json
 import yaml
 import allure
 
+from base.baseReadFile import BaseReadFile
+
 
 @allure.feature('请求项目管理分页查询接口')
 class TestDemo:
 
     @allure.story('查询项目管理')
     def test_url(self):
-        base_url = yaml.load(open("data.yaml"))["pangu_dev"]
+        base_url = BaseReadFile(r'\test_data\data.yaml').readFile()["pangu_dev"]
         data = json.dumps({"pageCount": 20, "pageNumber": 1, "pageIs": True})
         headers = {"Content-Type": "application/json;charset=UTF-8"}
         r = requests.post(base_url+"/ht-pangu-manager/web/project/page", data=data, headers=headers)
