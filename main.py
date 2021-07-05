f6301f0485de63f30011db45bf9421c51d989b2e
ct <2695562503@qq.com>
2021-07-05 16:23:20 +0800

    update 配置

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    pytest.main(["./"])
+    pytest.main(["--alluredir=report/result"])
     os.system(f"allure generate report/result -o report/allure_html --clean")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
